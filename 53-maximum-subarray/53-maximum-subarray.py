1ffc1225effc8d1d7ad89f761650705242c366f4
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-01-14 02:29:48 +0530

    Time: 1293 ms (10.15%), Space: 28.9 MB (11.41%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -4,6 +4,6 @@ class Solution:
         maximum = -10001
         for i in nums:
             total += i
-            maximum = max(total, maximum)
+            maximum = total if total > maximum else maximum 
             total = 0 if total < 0 else total
         return maximum
