cb66ce2cdc8c8e40858e374cfbc7515ba9dfd915
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-08-28 18:12:57 -0500

    Time: 639 ms (59.49%), Space: 25.9 MB (91.33%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        # Create a dictionary for storing the array elements (initially the dictionary will be empty). Traverse through the array and if the element exists in the dictionary return True, else add the element in the dictionary. Return False if the entire array is traversed
+        
         element_dict = {}
         for each_element in nums:
             if each_element in element_dict:
