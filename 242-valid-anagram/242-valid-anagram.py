f4cf4312adcff0d6a159233176d903cc3d3bbc43
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-08-31 23:58:36 -0500

    Time: 117 ms (8.80%), Space: 14.5 MB (67.18%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,19 +1,29 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):    return False
-        s_dict, t_dict = {}, {}
-        for i in range(len(s)):
-            if s[i] not in s_dict:
-                s_dict[s[i]] = 1
-            else:
-                s_dict[s[i]] += 1
-            if t[i] not in t_dict:
-                t_dict[t[i]] = 1
-            else:
-                t_dict[t[i]] += 1
-        for each_letter in s_dict:
-            if each_letter not in t_dict or s_dict[each_letter] != t_dict[each_letter]:
+        letter_array = [0]*26
+        for each_letter in s:
+            letter_array[ord(each_letter) - ord('a')] += 1
+        for each_letter in t:
+            letter_array[ord(each_letter) - ord('a')] -= 1
+        for i in letter_array:
+            if i != 0:
                 return False
         return True
+        
+        # if len(s) != len(t):    return False
+        # s_dict, t_dict = {}, {}
+        # for i in range(len(s)):
+        #     if s[i] not in s_dict:
+        #         s_dict[s[i]] = 1
+        #     else:
+        #         s_dict[s[i]] += 1
+        #     if t[i] not in t_dict:
+        #         t_dict[t[i]] = 1
+        #     else:
+        #         t_dict[t[i]] += 1
+        # for each_letter in s_dict:
+        #     if each_letter not in t_dict or s_dict[each_letter] != t_dict[each_letter]:
+        #         return False
+        # return True
                 
                 
