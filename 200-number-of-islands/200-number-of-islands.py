0653d81a0fa4d5aed3114673a227eb26a8132c88
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-10 12:37:13 -0500

    Time: 744 ms (10.70%), Space: 16.5 MB (48.16%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,11 +1,13 @@
 class Solution:
     """
-    
+    Traverse through the grid, if 1 is found - increment the count and call DFS()
+    Return back if the indexes are invalid or the grid element is 0
+    Else mark the current grid element as 0 and do DFS (horizontal and vertical) on the adjacent elements
     """
     def numIslands(self, grid: List[List[str]]) -> int:
         count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
+            for j in range(len(grid[i])):
                 if grid[i][j] == '1':
                     count += 1
                     self.dfs(i, j, grid)
