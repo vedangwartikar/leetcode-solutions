a344611e0a624c028450cab2690830405c33ccef
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-09 19:12:58 -0500

    Time: 77 ms (83.57%), Space: 15.4 MB (8.73%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,5 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        create a dict to store target - current element. 
+        first, if the current element has the counterpart in the dict, return the indexes
+        else add the target - current element to the dict
+        O(n)
+        """
         dict = {}
         for i, j in enumerate(nums):
             if j in dict:
