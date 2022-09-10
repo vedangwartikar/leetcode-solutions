f5d51fce069247c71a5c21614dadc4b130d798e1
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-10 14:14:40 -0500

    Time: 58 ms (27.98%), Space: 13.9 MB (26.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -5,7 +5,7 @@ class Solution:
         If the counterpart of the current paranthesis is not available in the stack append to the stack
         Else check if the counterpart i available - then pop the counterpart from the stack
         Return True is the stack is empty at the end, else False
-        * Return False directly if a closing bracket is found
+        * Return False directly if a closing bracket is found at first
         """
         if len(s) < 2: return False
         stack = []
