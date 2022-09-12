6545ae0df454b494909a3bc51b516f52426023e6
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-12 00:58:55 -0500

    Time: 301 ms (5.67%), Space: 19.9 MB (9.49%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,13 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashmap = {}
+        """
+        Create a count (a at 0, b at 25,... z at 25) for each string and map the counted array to each string
+        Match the counter array and store the anagram strings with the same key as counter
+        Counter stores key tuple([0,1,1,1,0,...]) with values ["bcd", "cdb", "dcb"]
+        Return the value of resultant list (without the count)
+        """
+        result = defaultdict(list)
         for string in strs:
-            sorted_string = ''.join(sorted(string))
-            if sorted_string not in hashmap:
-                hashmap[sorted_string] = [string]
-            else:
-                hashmap[sorted_string].append(string)
-        result = []
-        for i in hashmap:
-            result.append(hashmap[i])
-        return result
+            count = [0] * 26
+            for char in string:
+                count[ord(char) - ord('a')] += 1
+            result[tuple(count)].append(string)
+        return result.values()
+            
