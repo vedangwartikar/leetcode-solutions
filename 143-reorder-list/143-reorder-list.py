5e947589584713fa4af4106735cc7daf785fedb3
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-22 01:44:41 -0500

    Time: 243 ms (5.12%), Space: 23.9 MB (63.85%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -8,6 +8,12 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        """
+        1 -> 2 -> 3 -> 4 -> 5 -> None
+        Get the middle+1 of the linked list (4)
+        Reverse the linked list from that point (5 -> 4 -> None)
+        Reorder both the lists (first -> next = second; second.next = fnext; and so on)by traversing till the second list node is not None (5 -> 4 -> 3 -> 2 -> 1 -> None)
+        """
         slow, fast = head, head
         while slow.next and fast.next and fast.next.next:
             slow = slow.next
