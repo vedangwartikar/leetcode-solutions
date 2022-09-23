038a0ee138eeab0c04d17c92e22ae46cb049ecc4
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-09-22 21:30:43 -0500

    Time: 35 ms (87.64%), Space: 14.2 MB (31.61%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,3 +1,11 @@
+"""
+Create a hashmap of digits and their strings
+Create a backtracking function that appends generated_str to resultant array if the len(generated_str) is same as len(digits) and then returns back
+Else, for each character in the digit string call the backtracking with the next digit's index (index + 1) and updated string (curr_str + char of new string)
+Call backtrack with index 0 and an empty string if digits string exists
+In the end return combinations array
+O(4^n)
+"""
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         digits_map = {
@@ -18,6 +26,7 @@ class Solution:
                 combinations.append(curr_str)
                 return
             for char in digits_map[digits[i]]:
+                print(i + 1, curr_str + char)
                 backtrack(i + 1, curr_str + char)
         
         if digits:
