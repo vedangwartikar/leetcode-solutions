f8765737c84fd1de2ec82ace1169b0e853ceb2f0
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-10-19 13:32:16 -0500

    Time: 3469 ms (39.40%), Space: 14 MB (97.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,3 +1,7 @@
+# Initialize a 1-D dp array with +inf values, set fp[0] as 0
+# Iterate through each amount and each coin
+# Set the dp[a] with the minumum of current dp[a] or 1 + dp[a-c] i.e for amount 7 with coins 1,3,4,5 dp[4] will be either the already computed dp[4] or 1 + dp[3]
+# In the end return dp[amount] if it is different than previously set +inf value
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [1e9] * (amount + 1)
