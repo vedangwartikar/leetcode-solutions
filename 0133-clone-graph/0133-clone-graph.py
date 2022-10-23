2b366361a78504c062afe4e79de65e00d4d8c426
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-10-23 14:52:37 -0500

    Time: 37 ms (96.95%), Space: 14.4 MB (77.60%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,4 +1,11 @@
 """
+Create a hashmap old_to_new to main the key: value as old_node: new_node
+Traverse the nodes using dfs, if the old is already in the hashmap return its new node else create a copy and add it to the hashmap
+For each neighbor of the old node, append the dfs(neighbor) to copy's neighbor
+O(V+E)
+"""
+
+"""
 # Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
