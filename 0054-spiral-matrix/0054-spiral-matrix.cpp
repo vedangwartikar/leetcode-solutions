85c7dee1b7fd3cc65ee6613cc71a740174fd3d88
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-24 17:21:09 -0600

    Time: 4 ms (27.20%), Space: 6.9 MB (72.40%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -10,26 +10,30 @@ public:
         while (left < right && top < bottom) {
             int i;
             
-            //get all elements from left to right
+            // get all elements from left to right
             for(i = left; i < right; i++) {
                 spiral.push_back(matrix[top][i]);
             }
             top += 1;
             
+            // get all elements from top to bottom
             for (i = top; i < bottom; i++) {
                 spiral.push_back(matrix[i][right - 1]);
             }
             right -= 1;
-                
+            
+            // break incase the pointers cross
             if (!(left < right && top < bottom)) {
                 break;
             }
-                
+            
+            //get all elements from right to left
             for (i = right - 1; i > left - 1; i--) {
                 spiral.push_back(matrix[bottom - 1][i]);
             }
             bottom -= 1;
             
+            // get all elements from bottom to top
             for (i = bottom - 1; i > top - 1; i--) {
                 spiral.push_back(matrix[i][left]);
             }
