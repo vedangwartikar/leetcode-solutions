845feea75326ae2b3600e77e6451a197204d2dff
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-24 18:51:52 -0600

    Time: 7 ms (17.79%), Space: 8.3 MB (71.42%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,17 +4,17 @@ public:
         int low = 0, mid = 0, high = nums.size() - 1;
         int temp = 0;
         while (mid <= high) {
-            if (nums[mid] == 0) {
+            if (nums[mid] == 0) { // if element is 0, swap low - mid and increment low and mid
                 temp = nums[low];
                 nums[low] = nums[mid];
                 nums[mid] = temp;
                 low += 1;
                 mid += 1;
             }
-            else if (nums[mid] == 1) {
+            else if (nums[mid] == 1) { // if element is 1, increment mid
                 mid += 1;
             }
-            else { // element == 2
+            else { // if element is 2, swap mid - high and decrement high
                 temp = nums[mid];
                 nums[mid] = nums[high];
                 nums[high] = temp;
