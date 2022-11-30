8e3982543b3cc0f0fa692cddc470e8bd9a7e5704
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-29 19:19:10 -0600

    Time: 209 ms (59.27%), Space: 72.9 MB (63.96%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {
-        int n = s.size();
-        vector <vector <int>> dp(n, vector<int>(n, 0));
-        for (int i = n-1; i >= 0; i--) {
-            dp[i][i] = 1;
-            for (int j = i + 1; j < n; j++) {
-                if (s[i] == s[j]) {
-                    dp[i][j] = dp[i+1][j-1] + 2;
+        int n = s.size(); // string size
+        vector <vector <int>> dp(n, vector<int>(n, 0)); // initialize dp grid with 0s
+        for (int i = n-1; i >= 0; i--) { // get all indexes above the diagonal
+            dp[i][i] = 1; // each alphabet with itself makes a palindrome
+            for (int j = i + 1; j < n; j++) { // for loops such that we get indexes 23 12 13 01 02 03
+                if (s[i] == s[j]) { // if the characters match
+                    dp[i][j] = dp[i+1][j-1] + 2; // increment dp[i][j] with the count of alphabets matched (2)
                 }
                 else {
-                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]); // else it is max of the previous strings
                 }
             }
         }
-        return dp[0][n-1];
+        return dp[0][n-1]; // result is always present at the top right corner
     }
 };
