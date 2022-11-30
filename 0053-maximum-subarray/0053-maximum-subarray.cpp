001e29bc7ece1bba4aa56882674991ee035ab07e
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-29 22:45:04 -0600

    Time: 337 ms (26.50%), Space: 67.7 MB (55.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_sum = nums[0]; // max subarray sum is initially the first element
-        int current_sum = 0; // initialize the current sum to 0
-        for (auto element: nums) { // for each element
-            if (current_sum < 0) { // if the current sum goes negative
-                current_sum = 0; // reset the current sum to 0 (exclude the negative numbers locally)
+        int max_subarray = nums[0];
+        int current_sum = 0;
+        for (auto element: nums) {
+            if (current_sum < 0) {
+                current_sum = 0;
             }
-            current_sum += element; // update the current sum with the current element
-            max_sum = max(max_sum, current_sum); // check if newly computed sum is greater than max sum
+            current_sum += element;
+            max_subarray = max(max_subarray, current_sum);
         }
-        return max_sum; // return the max sum
+        return max_subarray;
     }
 };
