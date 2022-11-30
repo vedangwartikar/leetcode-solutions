25187c813f0331fc6b55847601855703d18cad8d
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-30 16:05:18 -0600

    Time: 784 ms (95.35%), Space: 75.5 MB (57.20%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,22 +1,22 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.cache = OrderedDict()
+        self.capacity = capacity # max capacity of cache
+        self.cache = OrderedDict() # ensures LRU at  beginning and MRU at the end
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        val = self.cache[key]
-        self.cache.move_to_end(key)
-        return val
+        if key not in self.cache: # key not present in cache
+            return -1 # return -1
+        val = self.cache[key] # get the value of the key from cache
+        self.cache.move_to_end(key) # shift the key to the end of the cache (MRU)
+        return val # return the value of the key
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            del self.cache[key]
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last = False)
+        if key in self.cache: # if key is already present
+            del self.cache[key] # delete the key: value pair
+        self.cache[key] = value # insert the updated key: value pair
+        if len(self.cache) > self.capacity: # if the length of the cache exceeds capacity
+            self.cache.popitem(last = False) # remove the key: value pair at the beginning (LRU)
 
 
 # Your LRUCache object will be instantiated and called as such:
