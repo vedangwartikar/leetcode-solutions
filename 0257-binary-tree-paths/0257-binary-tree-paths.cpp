ecb3ed45545f9e59deae93d8045bb12274560c6d
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-11-30 01:05:00 -0600

    Time: 4 ms (78.91%), Space: 12.9 MB (73.88%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -12,24 +12,25 @@
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        if (root == NULL) {
-            return {};
+        if (root == NULL) { // if no root
+            return {}; // return no paths
         }
-        vector<string> paths;
-        path(root, "", paths);
-        return paths;    
+        vector<string> paths; // paths
+        path(root, "", paths); // define a helper function that can be called recursively
+        return paths; // return the paths
     }
     
-    void path(TreeNode* root, string current_path, vector<string> &paths) {
-        current_path += to_string(root->val);
-        if (root->left != NULL) {
-            path(root->left, current_path + "->", paths);
+    void path(TreeNode* root, string current_path, vector<string> &paths) { // recursive helper function
+        current_path += to_string(root->val); // add the current root's value
+        
+        if (root->left != NULL) { // if root has a left
+            path(root->left, current_path + "->", paths); // recursively call left subtree to add the value in the future
         }
-        if (root->right != NULL) {
-            path(root->right, current_path + "->", paths);
+        if (root->right != NULL) { // if root has a right
+            path(root->right, current_path + "->", paths); // recursively call left subtree to add the value in the future
         }
-        if (root->left == NULL && root->right == NULL) {
-            paths.push_back(current_path);
+        if (root->left == NULL && root->right == NULL) { // if a leaf node
+            paths.push_back(current_path); // add the current computed path string to the result
         }
     }
 };
