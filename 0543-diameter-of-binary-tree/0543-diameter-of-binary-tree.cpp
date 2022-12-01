c4a180c1b3014211c66edc75818c7bec78be5cbe
Vedang Wartikar <vedangwartikar17@gmail.com>
2022-12-01 00:16:36 -0600

    Time: 25 ms (31.51%), Space: 20.3 MB (65.69%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,18 +12,19 @@
 class Solution {
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        int diameter = 0;
-        helper(root, diameter);
-        return diameter;
+        int diameter = 0;											// initially diameter is 0
+        helper(root, diameter);										// call recursive helper function
+        return diameter;											// return the max diameter
     }
     
-    int helper(TreeNode* root, int &diameter) {
-        if (!root) {
-            return 0;
+    int helper(TreeNode* root, int &diameter) {						// recursive helper function
+        if (!root) {												// if root is not NULL
+            return 0;												// return 0
         }
-        int left_diameter = helper(root->left, diameter);
-        int right_diameter = helper(root->right, diameter);
-        diameter = max(diameter, left_diameter + right_diameter); // maintain the diameter here (diameter is basically left height + right height)
-        return max(left_diameter, right_diameter) + 1;
+        int left_height = helper(root->left, diameter);				// get the left height
+        int right_height = helper(root->right, diameter);			// get the right height
+        diameter = max(diameter, left_height + right_height); 		// maintain the diameter here (diameter is basically left height + right height)
+        
+        return 1 + max(left_height, right_height);
     }
 };
